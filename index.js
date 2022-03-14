7acdb67c264850c22622a9b007c9855456d503dd
DanUgbeye <82518175+DanUgbeye@users.noreply.github.com>
2022-03-14 09:57:18 +0100

    implementing details page

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ const country = new COUNTRY();
 const ui = new UI();
 
 const showCountries = async () => {
+  
   const res = await country.get('https://restcountries.com/v2/all');
-  if(res.error) {
-    ui.showError(res.error); 
-    return; 
+  if(res != undefined) {
+    if(res.error) {
+      ui.showError(res.error); 
+      return;
+    }
   }
-  const data = await res.data;
-  const countries = await ui.loadCountries(data);
-  ui.displayCountries(countries);
+    
+  if(res) {
+    const data = await res.data;
+    const countries = await ui.loadCountries(data);
+    ui.displayCountries(countries);
+  }
 
 }
 
