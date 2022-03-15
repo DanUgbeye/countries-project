0987f8df02db5b6eb8791509952c65fa28b97d59
DanUgbeye <82518175+DanUgbeye@users.noreply.github.com>
2022-03-15 23:43:35 +0100

    fixed the image size bug

--- dist/scripts/ui.js
+++ dist/scripts/ui.js
@@ -12,27 +12,27 @@ const UI = {
       countries += `
       
       <!-- ${(data[i].name).toUpperCase()} -->
-      <a href="#" id="country" data-country="${data[i].alpha3Code}" data-id="${i}" class=" w-fit max-w-[15rem] shadow-md overflow-hidden rounded-md ">
+      <a href="#" id="country" data-country="${data[i].alpha3Code}" data-id="${i}" class=" w-fit max-w-[20rem] shadow-md overflow-hidden rounded-md ">
 
-        <img src="${data[i].flag}" alt="${data[i].name} flag" class=" min-w-[15rem] max-h-[7.5rem] min-h-[7.5rem] " loading="lazy" />
+        <img src="${data[i].flag}" alt="${data[i].name} flag" class=" min-w-[20rem] max-h-[10rem] min-h-[10rem] " loading="lazy" />
 
-        <div class=" p-4 bg-[hsl(0_0%_100%)] ">
+        <div class=" p-8 bg-[hsl(0_0%_100%)] ">
 
           <h3 id="country-name" class=" font-[600] text-xl mb-3 ">
             ${data[i].name}
           </h3>
 
-          <p class=" font-[600] text-[14px]  ">
+          <p class=" font-[600] mb-2 text-[14px]  ">
             Population: 
             <span id="population" class=" font-light ">${data[i].population}</span> 
           </p>
 
-          <p class=" font-[600] text-[14px] ">
+          <p class=" font-[600] mb-2 text-[14px] ">
             Region: 
             <span id="region" class=" font-light ">${data[i].region}</span> 
           </p>
 
-          <p class=" font-[600] text-[14px] ">
+          <p class=" font-[600] mb-2 text-[14px] ">
             Capital: 
             <span id="capital" class=" font-light ">${data[i].capital}</span>
           </p>
